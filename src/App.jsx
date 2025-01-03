90b15630b22c32b4a29936c314fede82009cc5af
Dhruv <dhruvgautam380@gmail.com>
2025-01-03 15:39:33 +0530

    Added React Router Dom and process routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import Pokidex from './components/Pokedex/Pokedex'
+import { Route, Routes } from 'react-router-dom'
+import PokemonDetails from './components/PokemonDetails/PokemonDetails'
+import Pokedex from './components/Pokedex/Pokedex'
 
 function App() {
 
 
   return (
     <>
-      <div>
-        <Pokidex/>
-      </div>
+      <Routes>
+        {/* <Pokidex/> */}
+        <Route path="/"  element={<Pokedex/>} />
+        <Route path="/pokemon/:id"  element={<PokemonDetails/>} />
+        <Route path="*"  element={<h1>NOT FOUND</h1>}/>
+      </Routes>
     </>
   )
 }
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,12 +1,15 @@
 // CSS imports
+import { Link } from "react-router-dom";
 import "./Pokemon.css"
 
-function Pokemon({name,image, url}){
+function Pokemon({name, url,id}){
   return(
+      <Link to={`/pokemon/${id}`}className="pokemon-wrapper">
     <div className="pokemon">
-      <div className="pokemon-name">{name}</div>
-      <img className="pokemon-image " src={url} />
+        <div className="pokemon-name">{name}</div>
+        <img className="pokemon-image " src={url} />
     </div>
+      </Link>
   )
 }
 
