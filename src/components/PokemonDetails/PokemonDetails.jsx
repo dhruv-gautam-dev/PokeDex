eac505393e01a75e5d22ea15c9f3d427fb81b2a8
Dhruv <dhruvgautam380@gmail.com>
2025-01-06 14:54:34 +0530

    Added custom hooks

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,67 +1,43 @@
 // imports CSS
-import { useEffect, useState } from "react";
 import "./PokemonDetails.css";
 import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+import usePokemon from "../../hooks/usePokemon";
 
-function PokemonDetails(){
-  const {id} = useParams();
-  const   POKEMON_DETAIL_URL ='https://pokeapi.co/api/v2/pokemon/';
-  const [pokemon,setPokemon] = useState(null);
+function PokemonDetails() {
+  const { id } = useParams();
 
-  async function downloadPokemons(){
-    const response = await axios.get(POKEMON_DETAIL_URL +id);
-    const pokemon = response.data;
-    setPokemon({
-      name:pokemon.name,
-      height:pokemon.height,
-      weight: pokemon.weight,
-      types:pokemon.types,
-      image:pokemon.sprites.other.dream_world.front_default
-    });
-  }
-
-  useEffect(()=>{
-    downloadPokemons();
-  },[]);
-
+  // custom hook
+  const [pokemon] = usePokemon(id);
 
   return (
     <>
-    <div className="pokedex-redirect">
-      <h1>
-        <Link to="/">
-        pokedex
-        
-        </Link>
-      </h1>
-    </div>
-      {pokemon && <div className="pokemon-details-wrapper">
-        <div className="pokemon-detail-name">
-          {pokemon.name}
-        </div>
-        <div className="pokemon-image">
-          <img src={pokemon.image} />
-
-        </div>
-        <div className="pokemon-atr">
-          <div>
-            height:{pokemon.height}
+      <div className="pokedex-redirect">
+        <h1>
+          <Link to="/">pokedex</Link>
+        </h1>
+      </div>
+      {pokemon && (
+        <div className="pokemon-details-wrapper">
+          <div className="pokemon-detail-name">{pokemon.name}</div>
+          <div className="pokemon-image">
+            <img src={pokemon.image} />
           </div>
-          <div>
-            weight:{pokemon.weight}
+          <div className="pokemon-atr">
+            <div>height:{pokemon.height}</div>
+            <div>weight:{pokemon.weight}</div>
           </div>
-
-        </div  >
-        <div className="pokemon-type-warapper" >
-        <h1>Type:</h1>
-        <div className="pokemon-types">
-          {pokemon.types.map(t=>  <span key={t.type.name}>{t.type.name}</span>)}
+          <div className="pokemon-type-warapper">
+            <h1>Type:</h1>
+            <div className="pokemon-types">
+              {pokemon.types.map((t) => (
+                <span key={t.type.name}>{t.type.name}</span>
+              ))}
+            </div>
+          </div>
         </div>
-        </div>
-    </div>}
+      )}
     </>
-  )
+  );
 }
 
 export default PokemonDetails;
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,62 +1,9 @@
 // // CSS imports
 import "./PokemonList.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import Pokemon from "../Pokemon/Pokemon";
+import usePokemonList from "../../hooks/usePokemonList";
 function PokemonList() {
-  const DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon";
-  // const [pokemonList,setPokemonList] =useState([]);
-  // const [pokedexUrl,setPokedexUrl] =useState(DEFAULT_URL);
-
-  // const [nextUrl ,setNextUrl] =useState(DEFAULT_URL);
-  // const [prevUrl ,setPrevUrl] =useState(DEFAULT_URL);
-
-  const [PokemonListState, setPokemonListState] = useState({
-    pokemonList: [],
-    pokedexUrl: DEFAULT_URL,
-    nextUrl: DEFAULT_URL,
-    prevUrl: DEFAULT_URL,
-  });
-
-  async function downloadPokemons() {
-    const response = await axios.get(
-      PokemonListState.pokedexUrl ? PokemonListState.pokedexUrl : DEFAULT_URL
-    );
-
-    const pokemonResults = response.data.results; // array of pokemons
-
-    // setNextUrl(response.data.next);
-    // setPrevUrl(response.data.previous);
-
-    // setPokemonListState((state)=>({...state,nextUrl: response.data.next, prevUrl: response.data.prevUrl}));
-
-    const pokemonPromise = pokemonResults.map((pokemon) =>
-      axios.get(pokemon.url)
-    );
-
-    const pokemonListData = await axios.all(pokemonPromise);
-
-    const pokemonFinalList = pokemonListData.map((pokemonData) => {
-      const pokemon = pokemonData.data;
-      return {
-        id: pokemon.id,
-        name: pokemon.name,
-        image: pokemon.sprites.other.dream_world.front_default,
-        types: pokemon.types,
-      };
-    });
-    // setPokemonList(pokemonFinalList);
-    setPokemonListState({
-      ...PokemonListState,
-      pokemonList: pokemonFinalList,
-      nextUrl: response.data.next,
-      prevUrl: response.data.previous,
-    });
-  }
-
-  useEffect(() => {
-    downloadPokemons();
-  }, [PokemonListState.pokedexUrl]);
+  const [PokemonListState, setPokemonListState] = usePokemonList();
 
   return (
     <div className="pokemon-list-wrapper">
